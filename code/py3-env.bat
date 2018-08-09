2b3a1d531d94b38aa485b5763b1493b05fa051e8
All4Gis <franka1986@gmail.com>
2018-08-09 23:12:15 +0200

    remove py3-env.bat from .gitignore

---
+++ code/py3-env.bat
@@ -0,0 +1,17 @@
+@ECHO OFF 
+
+set OSGEO4W_ROOT=D:\OSGeo4W64
+
+set PATH=%OSGEO4W_ROOT%\bin;%PATH%
+set PATH=%PATH%;%OSGEO4W_ROOT%\apps\qgis\bin
+
+@echo off
+call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
+call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
+call "%OSGEO4W_ROOT%\bin\py3_env.bat"
+@echo off
+path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
+
+cd /d %~dp0
+
+start cmd
