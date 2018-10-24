936ed279138242892504e9ff342fc599c4e8f4e5
All4Gis <franka1986@gmail.com>
2018-10-24 17:56:52 +0200

    remove copytree function

--- code/utils/QgsFmvUtils.py
+++ code/utils/QgsFmvUtils.py
@@ -561,17 +561,6 @@ def install_pip_requirements():
         raise
     return
 
-
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
-
-
 def rad2deg(radians):
     ''' Radians to Degrees '''
     degrees = 180 * radians / pi
