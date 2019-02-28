babe67cc93f71b60abb8a483662737a08e8305dd
ltbam <remy.baud@swisstopo.ch>
2019-02-28 12:11:51 +0100

    config fix

--- code/fmvConfig.py
+++ code/fmvConfig.py
@@ -4,15 +4,15 @@
 Platform_lyr = "Platform"
 Beams_lyr = "Beams"
 Footprint_lyr = "Footprint"
+Trajectory_lyr = "Trajectory"
 FrameCenter_lyr = "Frame Center"
 FrameAxis_lyr = "Frame Axis"
-Trajectory_lyr = "Trajectory"
 Point_lyr = "Drawings Point"
 Line_lyr = "Drawings Line"
 Polygon_lyr = "Drawings Polygon"
 
 # File extensions (first is default)
-Exts=["ts","mpeg4","mp4","avi","mpg","H264","mov"]
+Exts = ["ts", "mpeg4", "mp4", "avi", "mpg", "H264", "mov", "mpeg"]
 
 # Layers EPSG
 epsg = 'EPSG:4326'
@@ -20,17 +20,19 @@ epsg = 'EPSG:4326'
 # Group Name
 frames_g = "FMV Georeferenced Frames"
 
-#DTM (Digital terrain Model)
-DTM_file = "C:\\Program Files\\KadasAlbireo\\share\\kadas\\dtm_globe.tif"
-#Raster square size in pixel that will be loaded from DTM
-DTM_buffer_size = 5000
+# DTM (Digital terrain Model - look data folder globe.tif)
+# Change it using your path
+DTM_file = "D:\\GitHub\\QGISFMV\\data\\SRTM\\SRTM_W_250m.tif"
+# Raster square size in pixel that will be loaded from DTM
+DTM_buffer_size = 80
 
-#Reverse geocoding service that transforms point to address
-Reverse_geocoding_url="https://nominatim.openstreetmap.org/reverse.php?format=json&lat={}&lon={}"
+# Reverse geocoding service that transforms point to address
+Reverse_geocoding_url = "https://nominatim.openstreetmap.org/reverse.php?format=json&lat={}&lon={}"
 
 # FFmpeg path
-ffmpeg = "C:\\LegacySW\\kadas-portable_1.2.0.0\\kadas\\opt\\ffmpeg"
-ffprobe = "C:\\LegacySW\\kadas-portable_1.2.0.0\\kadas\opt\\ffmpeg"
+ffmpeg = "D:\\FFMPEG"
+ffprobe = "D:\\FFMPEG"
 
-# Buffer Metadata Reader Size (IMPORTANT : IF THIS VALUE IS VERY HIGH THE PLUGIN WILL FAIL)
+# Buffer Metadata Reader Size (IMPORTANT : IF THIS VALUE IS VERY HIGH THE
+# PLUGIN WILL FAIL)
 min_buffer_size = 8
