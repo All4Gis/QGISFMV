88f3945fba7bfd008d40e64e42774264c95abac7
Fran <franka1986@gmail.com>
2019-09-08 10:11:47 +0200

    Add deploy folder

---
+++ code/Compile.bat
@@ -0,0 +1,32 @@
+@ECHO OFF 
+cd /d %~dp0
+
+call "py3-env.bat"
+ 
+@ECHO ON
+
+::Ui Compilation
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_ColorDialog.ui -o gui\ui_ColorDialog.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvAbout.ui -o gui\ui_FmvAbout.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvManager.ui -o gui\ui_FmvManager.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvMetadata.ui -o gui\ui_FmvMetadata.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvMultiplexer.ui -o gui\ui_FmvMultiplexer.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvOpenStream.ui -o gui\ui_FmvOpenStream.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvOptions.ui -o gui\ui_FmvOptions.py  
+call pyuic5 --import-from QGIS_FMV.gui ui\ui_FmvPlayer.ui -o gui\ui_FmvPlayer.py  
+
+::Resources
+call pyrcc5 ui\resources.qrc -o gui\resources_rc.py
+
+::Translations
+
+@ECHO OFF
+GOTO END
+
+:ERROR
+   echo "Failed!"
+   set ERRORLEVEL=%ERRORLEVEL%
+   pause
+   
+:END
+@ECHO OFF
