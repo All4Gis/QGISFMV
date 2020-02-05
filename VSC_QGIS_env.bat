291743a626a9140a18189431d0d868404ab5bbaf
Fran Raga <All4Gis@users.noreply.github.com>
2020-02-05 12:25:36 +0100

    Create VSC_QGIS_env.bat

---
+++ VSC_QGIS_env.bat
@@ -0,0 +1,33 @@
+@echo off
+REM ***************************************************************************
+REM    VSC_QGIS_env.bat
+REM    ---------------------
+REM    begin                : June 2017
+REM    copyright            : (C) 2017 by Fran Raga
+REM    email                : franka1986 at gmail dot com
+REM ***************************************************************************
+REM *                                                                         *
+REM *   This program is free software; you can redistribute it and/or modify  *
+REM *   it under the terms of the GNU General Public License as published by  *
+REM *   the Free Software Foundation; either version 2 of the License, or     *
+REM *   (at your option) any later version.                                   *
+REM *                                                                         *
+REM ***************************************************************************
+
+:: QGIS development environment in Visual Studio Code for Windows
+
+set OSGEO4W_ROOT=D:\OSGeo4W64
+
+@echo off
+call %OSGEO4W_ROOT%\bin\o4w_env.bat
+call qt5_env.bat
+call py3_env.bat
+
+set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins
+set PATH=%PATH%;%OSGEO4W_ROOT%\apps\qgis\bin;%OSGEO4W_ROOT%\bin
+
+cd /d %~dp0
+
+SET VSCBIN=D:\Users\fran\AppData\Local\Programs\Microsoft VS Code
+
+call "%VSCBIN%\Code.exe %*
