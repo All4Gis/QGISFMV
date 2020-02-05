07513024ae4d00c9d22f65b4c956cfc5b61ee601
Fran Raga <All4Gis@users.noreply.github.com>
2020-02-05 12:23:59 +0100

    Update PyCharm_QGIS_env.bat

--- PyCharm_QGIS_env.bat
+++ PyCharm_QGIS_env.bat
@@ -18,9 +18,10 @@ REM ***************************************************************************
 
 set OSGEO4W_ROOT=D:\OSGeo4W64
 
-call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
-call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
-call "%OSGEO4W_ROOT%\bin\py3_env.bat"
+@echo off
+call %OSGEO4W_ROOT%\bin\o4w_env.bat
+call qt5_env.bat
+call py3_env.bat
 
 set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins
 set PATH=%PATH%;%OSGEO4W_ROOT%\apps\qgis\bin;%OSGEO4W_ROOT%\bin
