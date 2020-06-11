cce29c2b1d41c01d13881d84de6ac59e7edaf0dd
ltbam <remy.baud@swisstopo.ch>
2020-06-11 12:52:21 +0200

    Update setparser.py
    missing sys import

--- code/klvdata/setparser.py
+++ code/klvdata/setparser.py
@@ -23,6 +23,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import sys
 from abc import ABCMeta
 from abc import abstractmethod
 from collections import OrderedDict
