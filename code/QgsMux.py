43d54897de773ef6e285e020a9a76473545ad611
all4gis <franka1986@gmail.com>
2021-03-01 18:35:56 +0100

    tes gstreamer multiplexor

--- code/QgsMux.py
+++ code/QgsMux.py
@@ -1,48 +1,45 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import os
 import gi
+import cv2
 
+# Author : Fran Raga , 2021
+# proof of concept to ingest KLV telemetry into a video. Multiplexer concept to create a MISB Video.
+# Related with : https://github.com/All4Gis/QGISFMV/blob/master/code/manager/QgsMultiplexor.py
 
-# Video width/Height
+# Get Video width/Height
 # ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 DJI_0047.MP4 
 # width 3840x  height 2160
 
-# Get Framet rate
+# Get Frame rate
 # ffprobe -v error -select_streams v -of default=noprint_wrappers=1:nokey=1 -show_entries stream=r_frame_rate DJI_0047.MP4 
 
-# Extract frames
-# ffmpeg -i DJI_0047.MP4  -vf fps=1/29.97 %.1f.jpg
-# ffmpeg -i DJI_0047.MP4 -r 1/1 %1d.jpg
-
-# ffmpeg -i DJI_0047/DJI_0047.MP4 -i test.mp4 -c copy -map 0:v:0 -map 1:d:0 out.mp4
-
-
-
+# Merge all klv from QGISFMV to use in this code
 # import glob
 # 
 # files = []
-# for file in glob.glob("/home/fragalop/SamplesFMV/multiplex/klv/*.klv"):
+# for file in glob.glob("/home/fragalop/SamplesFMV/multiplexor/klv/*.klv"):
 #     files.append(file)
 # 
 # out_data = b''
 # for fn in files:
 #     with open(fn, 'rb') as fp:
 #         out_data += fp.read()
-# with open('/home/fragalop/SamplesFMV/multiplex/all.klv', 'wb') as fp:
+# with open('/home/fragalop/SamplesFMV/multiplexor/all.klv', 'wb') as fp:
 #     fp.write(out_data)
 
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst, GObject
 
-vid_frames_folder = "/home/fragalop/SamplesFMV/multiplex/frames"
-vid_frame_rate = 29.97
+vid_file = "/home/fragalop/SamplesFMV/multiplexor/DJI_0047/0.0.mp4"
+vid_frame_rate = 30
 vid_width = 3840
 vid_height = 2160
-klv_file = "/home/fragalop/SamplesFMV/multiplex/all.klv"
-klv_frame_rate = 30
-klv_packet_size = 129
-out_file = "/home/fragalop/SamplesFMV/multiplex/test2.mp4"
-
+klv_file = "/home/fragalop/SamplesFMV/multiplexor/all.klv"
+klv_packet_size = 112 # Extracted from QGISFMV "Create MISB Tool"
+out_file = "/home/fragalop/SamplesFMV/multiplexor/MISB.mp4"
+tmp_image= "/home/fragalop/SamplesFMV/multiplexor/tmp.jpg"
 
 GObject.threads_init()
 Gst.init(None)
@@ -131,62 +128,77 @@ queue_klv.link(muxer)
 pipeline.set_state(Gst.State.PLAYING)
 
 fh = open(klv_file, 'rb')
-timestamp = 0
 
-klv_done = False
-vid_done = False
+
 vid_frame_counter = 0
 klv_frame_counter = 0
 
-t = 0
-last_klv_t = 0
-last_vid_t = 0
-while True:
-    if vid_done and klv_done:
-        break
-    if t - last_klv_t >= 1.0 / klv_frame_rate:
-        if not klv_done:
+
+inject_klv = 0
+duration = 1.0 / vid_frame_rate * Gst.SECOND
+
+cap = cv2.VideoCapture(vid_file)
+
+while cap.isOpened():
+
+    try:
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        #data = frame.tostring()
+        # cap.get(cv2.CAP_PROP_POS_MSEC)\n
+        cv2.imwrite(tmp_image,frame)
+        # The result video has corrupted image, why?
+        # tmp fix using the saved image
+        vid_frame = plt.imread(tmp_image)
+        data = vid_frame.tostring()
+        vidbuf = Gst.Buffer.new_allocate(None, len(data), None)
+        vidbuf.fill(0, data)
+        vidbuf.duration = duration
+        timestamp = vid_frame_counter * duration
+        #timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+        vidbuf.pts = vidbuf.dts = int(timestamp)
+        vidbuf.offset = timestamp
+        retval = vsrc.emit("push-buffer", vidbuf)
+
+        if retval != Gst.FlowReturn.OK:
+            print(retval)
+        vid_frame_counter += 1
+        inject_klv +=1
+        print("video {}".format(vid_frame_counter))
+        
+        if inject_klv >= vid_frame_rate or vid_frame_counter==1:
+            # Inject KLV, We have 1 packet each second. 
             klv_bytes = fh.read(klv_packet_size)
-            if klv_bytes:
-                klvbuf = Gst.Buffer.new_allocate(None, klv_packet_size, None)
-                klvbuf.fill(0, klv_bytes)
-                klvbuf.pts = int(t * 1e9)
-                klvbuf.dts = int(t * 1e9)
+            klvbuf = Gst.Buffer.new_allocate(None, klv_packet_size, None)
+            klvbuf.fill(0, klv_bytes)
+            timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
+            klvbuf.duration = 1.0  * Gst.SECOND
+            klvbuf.pts = klvbuf.dts = int(timestamp)
+            klvbuf.offset = timestamp
+            retval = appsrc.emit("push-buffer", klvbuf)
+            if retval != Gst.FlowReturn.OK:
+                print(retval)
+            klv_frame_counter += 1
+            inject_klv=0
+            print("klv {}".format(klv_frame_counter))
+        
+    except Exception as e:
+        print("An exception occurred, video ",e)
+        break
+    
 
-                appsrc.emit("push-buffer", klvbuf)
-                klv_frame_counter += 1
-                last_klv_t = t
-                print("klv {} {}".format(klv_frame_counter, last_klv_t))
-            else:
-                klv_done = True
-
-    if t - last_vid_t >= 1.0 / vid_frame_rate:
-        if not vid_done:
-            frame_filename = '%s/%1d.jpg' % (vid_frames_folder, vid_frame_counter + 1)
-            if os.path.isfile(frame_filename):
-                vid_frame = plt.imread(frame_filename)
-                data = vid_frame.tostring()
-                vidbuf = Gst.Buffer.new_allocate(None, len(data), None)
-                vidbuf.fill(0, data)
-                vidbuf.pts = int(t * 1e9)
-                vidbuf.dts = int(t * 1e9)
-
-                vsrc.emit("push-buffer", vidbuf)
-                vid_frame_counter += 1
-                last_vid_t = t
-                print("vid {} {}".format(vid_frame_counter, last_vid_t))
-            else:
-                vid_done = True
-                continue
-
-    t += 0.000001
-    #print(t)
-
+print("end-of-stream")
 vsrc.emit("end-of-stream")
 appsrc.emit("end-of-stream")
+# Destroy openCV
+cap.release()
+# Remove temporal image
+os.remove(tmp_image)
 
+cv2.destroyAllWindows()
 bus = pipeline.get_bus()
-
+ 
 while True:
     msg = bus.poll(Gst.MessageType.ANY, Gst.CLOCK_TIME_NONE)
     t = msg.type
@@ -196,11 +208,11 @@ while True:
         pipeline.set_state(Gst.State.NULL)
     elif t == Gst.MessageType.ERROR:
         err, debug = msg.parse_error()
-        #print("Error: %s" % err, debug)
+        print("Error: %s" % err, debug)
         break
     elif t == Gst.MessageType.WARNING:
         err, debug = msg.parse_warning()
-        #print("Warning: %s" % err, debug)
+        print("Warning: %s" % err, debug)
     elif t == Gst.MessageType.STATE_CHANGED:
         pass
     elif t == Gst.MessageType.STREAM_STATUS:
@@ -208,7 +220,7 @@ while True:
     else:
         pass
         #print(t)
-        #print("Unknown message: %s" % msg)
+        print("Unknown message: %s" % msg)
 
 pipeline.set_state(Gst.State.NULL)
 
