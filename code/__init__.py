0951872c49056353d1603742840392c1aa261c49
Francisco Raga <francisco.raga.lopez@ext.esa.int>
2026-09-10 10:54:33 +0200

    replace ET

--- code/__init__.py
+++ code/__init__.py
@@ -26,7 +26,7 @@ def classFactory(iface):
 
     plugin = Fmv(iface)
 
-    if os.environ.get("FRAN_DEBUG") == "1":
+    if os.environ.get("QGISFMV_DEBUG") == "1":
         try:
             import debugpy
 
